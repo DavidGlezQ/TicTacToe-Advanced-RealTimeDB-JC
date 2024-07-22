7e11b4de3e6958aca91efdb5ca31082413ac04af
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2024-07-21 23:12:22 -0600

    Write in database

--- app/src/main/java/com/david/glez/firebasecourse/tictactoeadvancedrealtimedbjc/data/network/model/GameData.kt
+++ app/src/main/java/com/david/glez/firebasecourse/tictactoeadvancedrealtimedbjc/data/network/model/GameData.kt
@@ -5,12 +5,12 @@ import android.icu.util.Calendar
 data class GameData(
     val board: List<Int?>? = null,
     val gameId: String? = null,
-    val player1: Player? = null,
-    val player2: Player? = null,
-    val playerTurn: Player? = null
+    val playerData1: PlayerData? = null,
+    val playerData2: PlayerData? = null,
+    val playerDataTurn: PlayerData? = null
 )
 
-data class Player(
+data class PlayerData(
     val userId: String? = Calendar.getInstance().timeInMillis.hashCode().toString(),
     val playerType: Int? = null
 )
--- app/src/main/java/com/david/glez/firebasecourse/tictactoeadvancedrealtimedbjc/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/david/glez/firebasecourse/tictactoeadvancedrealtimedbjc/ui/home/HomeViewModel.kt
@@ -2,6 +2,8 @@ package com.david.glez.firebasecourse.tictactoeadvancedrealtimedbjc.ui.home
 
 import androidx.lifecycle.ViewModel
 import com.david.glez.firebasecourse.tictactoeadvancedrealtimedbjc.data.network.FirebaseService
+import com.david.glez.firebasecourse.tictactoeadvancedrealtimedbjc.data.network.model.GameData
+import com.david.glez.firebasecourse.tictactoeadvancedrealtimedbjc.data.network.model.PlayerData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -10,9 +12,19 @@ class HomeViewModel @Inject constructor(private val firebaseService: FirebaseSer
     ViewModel() {
 
     fun onCreateGame() {
+        firebaseService.createGame(gameData = createNewGame())
     }
 
     fun onJoinGame(gameId: String) {
     }
 
+    private fun createNewGame(): GameData {
+        val currentPlayer = PlayerData(playerType = 1)
+        return GameData(
+            board = List(9) { 0 },
+            playerData1 = currentPlayer,
+            playerData2 = null,
+            playerDataTurn = currentPlayer
+        )
+    }
 }
