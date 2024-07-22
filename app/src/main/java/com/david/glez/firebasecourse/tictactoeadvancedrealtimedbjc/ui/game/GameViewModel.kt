186551be47237e4f0a212c309da224c51501fdff
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2024-07-21 20:02:56 -0600

    Game module

---
+++ app/src/main/java/com/david/glez/firebasecourse/tictactoeadvancedrealtimedbjc/ui/game/GameViewModel.kt
@@ -0,0 +1,11 @@
+package com.david.glez.firebasecourse.tictactoeadvancedrealtimedbjc.ui.game
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GameViewModel @Inject constructor(): ViewModel() {
+
+
+}
