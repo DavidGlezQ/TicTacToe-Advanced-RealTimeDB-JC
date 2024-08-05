6ed1ba6e582a0fc524eb5d2da3a164a08caf1aef
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2024-08-05 09:45:04 -0600

    HomeScreenUi

--- app/src/main/java/com/david/glez/firebasecourse/tictactoeadvancedrealtimedbjc/ui/home/HomeScreen.kt
+++ app/src/main/java/com/david/glez/firebasecourse/tictactoeadvancedrealtimedbjc/ui/home/HomeScreen.kt
@@ -1,9 +1,18 @@
 package com.david.glez.firebasecourse.tictactoeadvancedrealtimedbjc.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
@@ -15,15 +24,27 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.david.glez.firebasecourse.tictactoeadvancedrealtimedbjc.R
+import com.david.glez.firebasecourse.tictactoeadvancedrealtimedbjc.ui.theme.Orange1
+import com.david.glez.firebasecourse.tictactoeadvancedrealtimedbjc.ui.theme.background
 
 @Composable
 fun HomeScreen(
     homeViewModel: HomeViewModel = hiltViewModel(),
     navigateToGame: (String, String, Boolean) -> Unit
 ) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(background)
+    ) {
+        Header()
+
         Spacer(modifier = Modifier.weight(2f))
 
         CreateGame {
@@ -45,6 +66,25 @@ fun HomeScreen(
         }
 
         Spacer(modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+fun Header() {
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .clip(RoundedCornerShape(24.dp))
+            .border(2.dp, Orange1, RoundedCornerShape(24.dp)),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "logo"
+        )
     }
 }
 
